cc3dd19f553930d63bfb7c5de8fabd927037f3a5
Crypoune <a.messenet@gmail.com>
2026-05-11 11:43:19 +0200

    fix res

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,8 +1,8 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('HEllo Holberton School!');
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School!');
 });
 
 app.listen(1245);
