0728f4a161dec620e7f51ddc777d333b81e3abb8
Crypoune <a.messenet@gmail.com>
2026-05-11 13:36:05 +0200

    fix task 5

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -10,31 +10,31 @@ function countStudents(path) {
       }
 
       const fields = {};
-      const students = data.split('\n').filter((student) => student.trim() !== '');
-      if (students.length === 0) {
-        reject(new Error('Cannot load the database'));
-        return;
-      }
+      const students = data
+        .split('\n')
+        .filter((student) => student.trim() !== '');
 
       students.shift();
 
       let response = `Number of students: ${students.length}`;
       console.log(response);
-      response += '\n';
 
       for (const student of students) {
         const cols = student.split(',');
         const field = cols[3];
+
         if (!fields[field]) {
           fields[field] = [];
         }
+
         fields[field].push(cols[0]);
       }
 
-      for (const i of Object.keys(fields)) {
-        const responseField = `Number of students in ${i}: ${fields[i].length}. List: ${fields[i].join(', ')}`;
-        response += `${responseField}\n`;
+      for (const field of Object.keys(fields)) {
+        const responseField = `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`;
+
         console.log(responseField);
+        response += `\n${responseField}`;
       }
 
       resolve(response);
